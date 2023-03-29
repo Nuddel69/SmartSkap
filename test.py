b0de806a1ee93595a6b8965b8131139081ae49f7
Nuddel69 <mats.bjonnes@gmail.com>
2023-03-29 11:26:52 +0200

    Modified test.py

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import serial
 import time
 from datetime import datetime
 
+posX: float = 0.0;
+posY: float = 0.0;
+
 def command(ser, command):
   start_time = datetime.now()
   ser.write(str.encode(command)) 
@@ -19,10 +22,45 @@ time.sleep(2)
 
 # Move
 command(ser, "G91\r\n") # rapid motion but does not extrude material
-command(ser, "G0 F10000 X14000 \r\n") # rapid motion but does not extrude material
+
+
+def scanBox():
+    # Code to scan using camera
+
+    # Return code of box
+    return "00F"
+
+
+def findBox(boxcode):
+    
+    coordX = 100
+    coordY = 100
+
+    # Return coordinates
+    return [coordX,coordY]
+
+
+
+def move(distX, distY) -> None:
+    global posX
+    global posY
+    # X direction (width)
+    command(ser, f"G0 F10000 Y{distX * 0} \r\n")
+
+    # Y direction (height)
+    command(ser, f"G0 F10000 X{distY * 31.847} \r\n")
+
+    posX += distX
+    posY += distY
+
+    print(posX)
+    print(posY)
+
 #command(ser, "G0 X350 \r\n") # rapid motion but does not extrude material ender 5 plus is 350 x 350
 #command(ser, "G1 F20000 Z0.564\r\n") # change layer
-#command(ser, "G0 X350 \r\n") # rapid motion but does not extrude material ender 5 plus is 350 x 350
 
+
+move(0, -70)
+move(0, 70)
 time.sleep(2)
 ser.close()
