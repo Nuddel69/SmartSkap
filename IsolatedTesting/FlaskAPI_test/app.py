670ba0884c3b81e0c588f9b70608c03d413f32f7
Sukhmani Kakar <sukhmanikakar11@gmail.com>
2023-04-13 17:26:28 +0200

    FIxed app.py

--- IsolatedTesting/FlaskAPI_test/app.py
+++ IsolatedTesting/FlaskAPI_test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,16 +6,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
 
 class Message(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	content = db.Column(db.String(200))
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
 
 @app.route('/save-message', methods=['POST'])
 def save_message():
-	message = request.json['message']
-	new_message = Message(content=message)
-	db.session.add(new_message)
-	db.session.commit()
-	return {'status': 'success'}
+    message = request.json['message']
+    new_message = Message(content=message)
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
