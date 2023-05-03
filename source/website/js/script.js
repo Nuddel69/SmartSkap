bf8b116f410e3264f5c2615c0be8e2af0d5fea33
ItsMeBrille <herhellum@gmail.com>
2023-05-03 20:02:01 +0200

    changes in script logic (currently not working)

--- source/website/js/script.js
+++ source/website/js/script.js
@@ -1,7 +1,6 @@
 // JavaScript
 
-// 
-
+// Basic functions
 function readCookie(name) {
   var nameEQ = name + "=";
   var ca = document.cookie.split(';');
@@ -19,6 +18,8 @@ function setCookie(name, value) {
   document.cookie = name+"="+value+"; expires="+date.toGMTString();
 }
 
+
+// Design logic
 
 // Inventory search
 const searchInput = document.getElementById('productsearch');
@@ -48,108 +49,30 @@ const showSidenavOptions = () => {
 
 
 
-// Add to cart
-// Functions
-
- var loadCart = function(){
-  // Retrieve cart from local storage
-  var cart = JSON.parse(localStorage.getItem('cart'));
-
-  if(cart == null){
-    cart = {};
-  }
-
-  if(!(username in cart)){
-    cart[username] = [];
-    // Store cart in local storage
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-
-  document.getElementById('cart-button').getElementsByTagName('span')[0].innerText = cart[username].length;
+// Catalog and cart logic
 
 
-  for (var i = 0; i < cart[username].length; i++){
-
-    element = document.getElementById(cart[username][i]);
-    $( "#cart-list" ).append( '<li class="cart-element list-group-item justify-content-between align-items-center" id="cart-' + element.id + '">' + element.getElementsByTagName('h1')[0].innerText + '<button class="btn"><i class="text-primary fa-regular fa-trash-can"></i></button></li>' );
-    document.getElementById('cart-'+element.id).getElementsByTagName('button')[0].addEventListener('click', removeFromCart, false);
-  }
-
-  return cart
+function addToCart(id){
+  cart.push(id)
 }
 
 
-var addToCart = function() {
-  
-  element = this.parentElement.parentElement;
-  
-  if(cart[username].includes(element.id)){return}
 
-  cart[username].push(element.id);
-  
-  // Store cart in local storage
-  localStorage.setItem('cart', JSON.stringify(cart));
-  
-  // Update cart number
-  document.getElementById('cart-button').getElementsByTagName('span')[0].innerText = cart[username].length;
-
-  $( "#cart-list" ).append( '<li class="cart-element list-group-item justify-content-between align-items-center" id="cart-' + element.id + '">' + element.getElementsByTagName('h1')[0].innerText + '<button class="btn"><i class="text-primary fa-regular fa-trash-can"></i></button></li>' );
-  document.getElementById('cart-'+element.id).getElementsByTagName('button')[0].addEventListener('click', removeFromCart, false);
-};
-
-
-
-var removeFromCart = function() {
-  
-  element = this.parentElement;
-
-  // Only continue if exist
-  if(!(cart[username].includes(element.id.substring(5)))){return}
-
-  const index =  cart[username].indexOf(element.id.substring(5));
-  if (index > -1) { // only splice array when item is found
-    cart[username].splice(index, 1); // 2nd parameter means remove one item only
+$(window).on('load', function() {
+  if(!readCookie("username")){
+    $('#logInModal').modal('show');
+    return;
   }
 
-  element.remove();
-
-  // Store cart in local storage
-  localStorage.setItem('cart', JSON.stringify(cart));
-
-  // Update cart number
-  document.getElementById('cart-button').getElementsByTagName('span')[0].innerText = cart[username].length;
-
-};
-
-
-
-var emptyCart = function() {
+  // User is logged in
+  username = readCookie("username");
   
-  element = document.getElementById("cart-list");
-
-  cart[username] = [];
-
-  // Store cart in local storage
-  localStorage.setItem('cart', JSON.stringify(cart));
-  
-  element.innerHTML = "";
-  
-  // Update cart number
-  document.getElementById('cart-button').getElementsByTagName('span')[0].innerText = cart[username].length;
-
-};
-
-
-function loadData(username) {
-  
-  $.getJSON(`catalog.json?user=${username}`, function(data) {
-    // JSON result in `data` variable
-    console.log(data);
-    console.log(data["catalog"]);
-    console.log(data["catalog"][0].name);
+  // Call API to get catalog
+  $.getJSON("./catalog.json", function(catalog){
 
     // Display products from returned JSON 
-    data["catalog"].forEach(product => {
+
+    catalog.forEach(product => {
 
       if(product.availability){
         var availability = `<span class="badge bg-success rounded-pill">Tilgjengelig</span>`;
@@ -157,9 +80,8 @@ function loadData(username) {
         var availability = `<span class="badge bg-danger rounded-pill">Ikke på lager</span>`;
       }
 
-      $( "#inventory-list" ).prepend(`<a class="inventory-element list-group-item justify-content-between align-items-center" data-bs-toggle="list" href="#product-${product.productid}">${product.name}${availability}</a>`);
-      
-      $( "#products" ).append(`<div class="product tab-pane" id="product-${product.productid}">
+      $( "#inventory-list" ).prepend(`<a class="inventory-element list-group-item justify-content-between align-items-center" data-bs-toggle="list" href="#pane-${product.id}">${product.name}${availability}</a>`);
+      $( "#products" ).prepend(`<div class="product tab-pane" id="pane-${product.id}">
         <div class="small mb-1">BIN: #${product.bin}</div>
         <h1 class="display-5 fw-bolder">${product.name}</h1>
         <p class="fs-5 mb-2">${product.category}</p>
@@ -167,52 +89,48 @@ function loadData(username) {
         <div class="d-flex" data-productid="${product.productid}">
             <button class="fav-product btn btn-dark" type="button"><i class="fa-solid fa-star"></i></button>
             &nbsp;
-            <button class="add-product btn btn-outline-dark" type="button"><i class="fa-solid fa-cart-shopping"></i> Legg til</button>
+            <button class="add-product btn btn-outline-dark" type="button" onClick="addToCart(${product.id})"><i class="fa-solid fa-cart-shopping"></i>&nbsp;Legg til</button>
         </div>
       </div>`);
+
+      $( `[data-productid="${product.id}"]` ).click(function(){
+        console.log(this);
+      });
     });
 
-    var products = document.getElementsByClassName('product');
 
-    for (var i = 0; i < products.length; i++) {
-      products[i].getElementsByClassName('add-product')[0].addEventListener('click', addToCart, false);
-    }
+
+
+    cart = [1, 2];
+
+    // Load Cart
+    $(".cart-nr").text = cart.length;
+    cart.forEach(i => {
+      product = catalog.find(x => x.id == i);
+      console.log
+
+      $( ".cart-list" ).append( `<li class="cart-element list-group-item justify-content-between align-items-center" data-productid="${product.id}">${product.name}<button class="btn"><i class="text-primary fa-regular fa-trash-can"></i></button></li>` );
+    });
+
+    $( ".cart-list" ).click(function(){
+      removeFromCart
+    });
+
+
+
+    console.log(catalog);
 
   });
 
-};
-
-
-
-/*
-<li class="cart-element list-group-item justify-content-between align-items-center" id="cart-product-2">20 ohm motstand<button class="btn"><i class="text-primary fa-regular fa-trash-can"></i></button></li>
-*/
-
-
-
-// Script
-
-var username;
-var cart;
-
-$(window).on('load', function() {
-  if(readCookie("username") == null){
-    $('#logInModal').modal('show');
-  } else {
-    username = readCookie("username");
-    
-    loadData(username);
-    cart = loadCart();
-  }
 });
+
+
+
 
 
 document.getElementById('logInButton').addEventListener('click', () => {
   setCookie("username", document.getElementById('logInName').value)
-  username = readCookie("username");
-
-  loadData(username);
-  cart = loadCart();
+  location.reload();
 });
 
 
