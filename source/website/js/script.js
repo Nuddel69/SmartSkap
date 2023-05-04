9622140796925fb7b52a9247f3e98780bd28e515
Brille7 <herhellum@gmail.com>
2023-05-04 09:19:16 +0200

    website now works

--- source/website/js/script.js
+++ source/website/js/script.js
@@ -48,12 +48,34 @@ const showSidenavOptions = () => {
 };
 
 
-
 // Catalog and cart logic
+
+var catalog;
+
+
+function removeFromCart(id){
+  // Remove from cart
+  const index = cart.indexOf(id);
+  if(index > -1){
+    cart.splice(index, 1);
+  }
+  // Remove from list
+  $( `li[data-productid="${id}"]` ).remove();
+  // Update number
+  $( ".cart-nr" ).text(cart.length);
+}
 
 
 function addToCart(id){
-  cart.push(id)
+  if(cart.includes(id)){
+    return;
+  }
+
+  cart.push(id);
+  product = catalog.find(x => x.id == id);
+  $( ".cart-list" ).append( `<li class="cart-element list-group-item justify-content-between align-items-center" data-productid="${product.id}">${product.name}<button class="btn" onClick="removeFromCart(${product.id})"><i class="text-primary fa-regular fa-trash-can"></i></button></li>` );
+
+  $( ".cart-nr" ).text(cart.length);
 }
 
 
@@ -68,8 +90,9 @@ $(window).on('load', function() {
   username = readCookie("username");
   
   // Call API to get catalog
-  $.getJSON("./catalog.json", function(catalog){
+  $.getJSON("./catalog.json", function(result){
 
+    catalog = result;
     // Display products from returned JSON 
 
     catalog.forEach(product => {
@@ -92,12 +115,7 @@ $(window).on('load', function() {
             <button class="add-product btn btn-outline-dark" type="button" onClick="addToCart(${product.id})"><i class="fa-solid fa-cart-shopping"></i>&nbsp;Legg til</button>
         </div>
       </div>`);
-
-      $( `[data-productid="${product.id}"]` ).click(function(){
-        console.log(this);
-      });
     });
-
 
 
 
@@ -109,43 +127,49 @@ $(window).on('load', function() {
       product = catalog.find(x => x.id == i);
       console.log
 
-      $( ".cart-list" ).append( `<li class="cart-element list-group-item justify-content-between align-items-center" data-productid="${product.id}">${product.name}<button class="btn"><i class="text-primary fa-regular fa-trash-can"></i></button></li>` );
+      $( ".cart-list" ).append( `<li class="cart-element list-group-item justify-content-between align-items-center" data-productid="${product.id}">${product.name}<button class="btn" onClick="removeFromCart(${product.id})"><i class="text-primary fa-regular fa-trash-can"></i></button></li>` );
     });
 
-    $( ".cart-list" ).click(function(){
-      removeFromCart
-    });
-
-
-
-    console.log(catalog);
+    $( ".cart-nr" ).text(cart.length);
 
   });
 
 });
 
 
-
-
-
+// Login
 document.getElementById('logInButton').addEventListener('click', () => {
   setCookie("username", document.getElementById('logInName').value)
   location.reload();
 });
 
 
-document.getElementById('remove-cart').addEventListener('click', () => {
-  emptyCart();
-  document.getElementById('offcanvasRight').classList.remove('show');
+// Save cart
+document.getElementById('save-cart').addEventListener('click', () => {
+  fetch('https://reqbin.com/echo/post/json', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "id": 78912 })
+  })
+  
+  .then(response => response.json())
+  .then(response => console.log(JSON.stringify(response)))
 });
 
 
 document.getElementById('order-cart').addEventListener('click', () => { 
   document.getElementById('offcanvasRight').classList.remove('show');
 
-  $( "#checkout-number" ).text(cart[username].length);
-  
   // Send results
+
+
+  cart.forEach(id => {
+    console.log(cart);
+    removeFromCart(id);
+  });
 
   $('#orderModal').modal('show');
 });
